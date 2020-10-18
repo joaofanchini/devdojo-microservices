b4d9de237e3362a3f6c135fb14cd431183b60cb8
Joao Victor Fanchini de Azevedo <joaofanchini@gmail.com>
2020-10-17 23:43:56 -0300

    Ajuste relacionado ao scanner de componentes

--- core/src/main/java/br/com/microsservices/academy/core/repositories/CourseRepository.java
+++ core/src/main/java/br/com/microsservices/academy/core/repositories/CourseRepository.java
@@ -1,9 +1,8 @@
 package br.com.microsservices.academy.core.repositories;
 
 import br.com.microsservices.academy.core.models.Course;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
-@Component
+@Repository
 public interface CourseRepository extends RepositoryBase<Course> {
 }
