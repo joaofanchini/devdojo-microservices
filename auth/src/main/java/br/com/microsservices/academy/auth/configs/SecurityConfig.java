635a5b6e0a679052d7f9cd3aaca964a382338be8
Joao Victor Fanchini de Azevedo <joaofanchini@gmail.com>
2020-10-18 18:32:47 -0300

    Add filtro de autorização, que será executado uma vez a cada requisição e após o filtro de autenticação

--- auth/src/main/java/br/com/microsservices/academy/auth/configs/SecurityConfig.java
+++ auth/src/main/java/br/com/microsservices/academy/auth/configs/SecurityConfig.java
@@ -2,7 +2,9 @@ package br.com.microsservices.academy.auth.configs;
 
 import br.com.microsservices.academy.auth.filters.AuthUsernamePasswordFilter;
 import br.com.microsservices.academy.core.properties.JwtProperty;
+import br.com.microsservices.academy.security.filters.JwtAuthorizationFilter;
 import br.com.microsservices.academy.security.helpers.TokenCreatorHelper;
+import br.com.microsservices.academy.security.helpers.TokenParserHelper;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Description;
@@ -11,18 +13,23 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity // Anotação necessária para aplicação do Spring Security
 @Description("Lembrar que a url por padrão com o Spring Security para o token é /login")
 public class SecurityConfig extends br.com.microsservices.academy.security.configs.SecurityConfig {
     private final TokenCreatorHelper tokenCreatorHelper;
+    private final TokenParserHelper tokenParserHelper;
     private final UserDetailsService userDetailsService;
 
     // O Qualifier pode ser o nome da propria classe de implementação em cammel case
-    public SecurityConfig(JwtProperty jwtProperty, TokenCreatorHelper tokenCreatorHelper, @Qualifier("userDetailsImplementation") UserDetailsService userDetailsService) {
+    public SecurityConfig(JwtProperty jwtProperty, TokenCreatorHelper tokenCreatorHelper,
+                          @Qualifier("userDetailsImplementation") UserDetailsService userDetailsService,
+                          TokenParserHelper tokenParserHelper) {
         super(jwtProperty);
         this.tokenCreatorHelper = tokenCreatorHelper;
         this.userDetailsService = userDetailsService;
+        this.tokenParserHelper = tokenParserHelper;
     }
 
     @Bean
@@ -34,8 +41,8 @@ public class SecurityConfig extends br.com.microsservices.academy.security.confi
     @Description("Configuração básica para Cors e modelos de autenticação")
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .addFilter(new AuthUsernamePasswordFilter(jwtProperty, authenticationManager(), tokenCreatorHelper)); // O Filtro que irá fazer nossa authenticação para cada requisição
-
+                .addFilter(new AuthUsernamePasswordFilter(jwtProperty, authenticationManager(), tokenCreatorHelper)) // O Filtro que irá fazer nossa authenticação para cada requisição
+                .addFilterAfter(new JwtAuthorizationFilter(jwtProperty,tokenParserHelper), UsernamePasswordAuthenticationFilter.class); // Filtro que será executado após o filtro indicado. Este filtro é o de Autorização
         super.configure(http);
     }
 
