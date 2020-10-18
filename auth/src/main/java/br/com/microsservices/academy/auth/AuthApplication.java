36fc9f1be580eeb2b8c5ac9b08a6a6e60660f0c3
Joao Victor Fanchini de Azevedo <joaofanchini@gmail.com>
2020-10-18 02:48:09 -0300

    Configs inciais para authenticação

--- auth/src/main/java/br/com/microsservices/academy/auth/AuthApplication.java
+++ auth/src/main/java/br/com/microsservices/academy/auth/AuthApplication.java
@@ -1,9 +1,14 @@
 package br.com.microsservices.academy.auth;
 
+import br.com.microsservices.academy.core.properties.JwtProperty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"br.com.microsservices.academy.core","br.com.microsservices.academy.auth"})
+@EnableConfigurationProperties(value = JwtProperty.class) // É mais correto esse cara ficar em uma classe de configuração
 public class AuthApplication {
 
 	public static void main(String[] args) {
---
+++ auth/src/main/java/br/com/microsservices/academy/auth/configs/SecurityConfig.java
@@ -0,0 +1,37 @@
+package br.com.microsservices.academy.auth.configs;
+
+import br.com.microsservices.academy.core.properties.JwtProperty;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Description;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableWebSecurity
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final JwtProperty jwtProperty;
+    private final UserDetailsService userDetailsService;
+
+    @Bean
+    private BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
+
+    @Override
+    @Description("Responsável por fazer a authenticação chamando o método findByUserName pelo userDetailsService")
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+}
